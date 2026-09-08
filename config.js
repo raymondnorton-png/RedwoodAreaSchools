a5cf3c909cb3dd52e096de1d17d22144a2d6ee26
raymondnorton-png <raymond.norton@swwc.org>
2026-09-08 09:41:22 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* Paste published viewform URLs from the spreadsheet tab "Form links". */
 window.RASD_SMS_FORMS = {
-  optin: "https://docs.google.com/forms/d/e/1FAIpQLSfV1Jrnnc_qBCS64JhizBNMbYGj4mVZ2esSANEFu-Rnio0kTA/viewform?usp=header"
-  help: "https://docs.google.com/forms/d/e/1FAIpQLScpiPddkDSM_F-dTvlDOg-Dlr6GXwT59B1hYIGgs_46_lmoQg/viewform?usp=header"
-  optout: "https://docs.google.com/forms/d/e/1FAIpQLSdGh0O1P8pj0xkPEEYHezPZtCGH8SMXrYuDE2L8k-_LswMUpw/viewform?usp=header"
+  optin: "https://docs.google.com/forms/d/e/1FAIpQLSfV1Jrnnc_qBCS64JhizBNMbYGj4mVZ2esSANEFu-Rnio0kTA/viewform",
+  help: "https://docs.google.com/forms/d/e/1FAIpQLScpiPddkDSM_F-dTvlDOg-Dlr6GXwT59B1hYIGgs_46_lmoQg/viewform",
+  optout: "https://docs.google.com/forms/d/e/1FAIpQLSdGh0O1P8pj0xkPEEYHezPZtCGH8SMXrYuDE2L8k-_LswMUpw/viewform"
 };
